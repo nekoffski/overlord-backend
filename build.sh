a4498ffe391b4cbbfb7adb8e8fca60930f6fcf38
nekoffski <kacperczyz@protonmail.com>
2024-11-16 09:16:33 +0100

    push docker on build

--- build.sh
+++ build.sh
@@ -2,29 +2,16 @@
 
 cwd=$(pwd)
 echo "current directory ->" $cwd
+echo "building..."
+services_path=$cwd/services
 
-function build {
-    echo "building..."
-    services_path=$cwd/services
-
-    for service_path in $services_path/*; do
-        service_name=$(basename $service_path)
-        echo "building" $service_name
-        pushd $service_path
-        docker image build -t overlord-$service_name:latest --file $service_path/Dockerfile .
-        popd
-    done
-}
-
-function deploy {
-    echo "deploying..."
-}
-
-if [ "$1" = '--build' ]; then
-    build
-elif [ "$1" = '--deploy' ]; then
-    deploy
-else
-    echo "ERROR: specify either --build or --deploy"
-    exit -1
-fi
+for service_path in $services_path/*; do
+    service_name=$(basename $service_path)
+    echo "building" $service_name
+    pushd $service_path
+    docker image build -t overlord-$service_name:latest --file $service_path/Dockerfile .
+    echo $DOCKER_TOKEN | docker login --username $DOCKER_USER --password-stdin
+    docker tag overlord-$service_name $DOCKER_USER/overlord-$service_name
+    docker push $DOCKER_USER/overlord-$service_name
+    popd
+done
