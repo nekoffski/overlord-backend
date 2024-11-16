fec850470b69d183f03123fa0b41fc2f1451e244
nekoffski <kacperczyz@protonmail.com>
2024-11-16 14:08:53 +0100

    use buildx

--- build.sh
+++ build.sh
@@ -5,13 +5,15 @@ echo "current directory ->" $cwd
 echo "building..."
 services_path=$cwd/services
 
+echo $DOCKER_TOKEN | docker login --username $DOCKER_USER --password-stdin
+docker buildx create --use
+
 for service_path in $services_path/*; do
     service_name=$(basename $service_path)
     echo "building" $service_name
     pushd $service_path
-    docker image build -t overlord-$service_name:latest --file $service_path/Dockerfile .
-    echo $DOCKER_TOKEN | docker login --username $DOCKER_USER --password-stdin
-    docker tag overlord-$service_name $DOCKER_USER/overlord-$service_name
-    docker push $DOCKER_USER/overlord-$service_name
+    docker buildx build --push --platform linux/arm64/v8 \
+        --tag $DOCKER_USER/overlord-$service_name:latest --file $service_path/Dockerfile .
+
     popd
 done
