854626eda170c324798cade78aa02adcd670c14e
nekoffski <kacperczyz@protonmail.com>
2024-11-19 17:38:08 +0100

    implement log-server service

--- build.sh
+++ build.sh
@@ -4,16 +4,21 @@ cwd=$(pwd)
 echo "current directory ->" $cwd
 echo "building..."
 services_path=$cwd/services
+push=false
 
 echo $DOCKER_TOKEN | docker login --username $DOCKER_USER --password-stdin
-docker buildx create --use
 
 for service_path in $services_path/*; do
     service_name=$(basename $service_path)
     echo "building" $service_name
     pushd $service_path
-    docker buildx build --push --platform linux/arm64/v8 \
-        --tag $DOCKER_USER/overlord-$service_name:latest --file $service_path/Dockerfile .
 
+    docker image build -t overlord-$service_name:latest --file $service_path/Dockerfile .
+    docker tag overlord-$service_name $DOCKER_USER/overlord-$service_name
+
+    if [ "$my_bool" = true ]; then
+        docker push $DOCKER_USER/overlord-$service_name
+    fi
+    
     popd
 done
