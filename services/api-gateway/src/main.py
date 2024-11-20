ad6a6d09d58a67d3cb9a8949c32a692e1dcc6ec0
nekoffski <kacperczyz@protonmail.com>
2024-11-20 20:59:43 +0100

    Handle ping requests in api-gateway

--- services/api-gateway/src/main.py
+++ services/api-gateway/src/main.py
@@ -2,10 +2,14 @@ import asyncio
 
 from overlord.log import log, setup_logger
 
+import api
+
 
 async def main():
     setup_logger(service_name='overlord-api-gateway')
     log.info("Service starting")
+
+    await api.start()
 
 
 if __name__ == '__main__':
